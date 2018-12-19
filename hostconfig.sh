3c74c5b7b34c8bf813f7c0b0dc51f217d7f032ff
azure_cernoel <gombocz@student.tugraz.at>
2018-12-19 18:55:23 +0000

    changed from pgadmin ssl to pgadmin without, could not master openssl

--- hostconfig.sh
+++ hostconfig.sh
@@ -1,3 +1,11 @@
 # configures the host, to forward exposed ports
 # ! every port that docker exposes gets exposed in the real world !
 sudo iptables -P FORWARD ACCEPT
+
+# https://development.robinwinslow.uk/2016/06/23/fix-docker-networking-dns/
+touch /etc/docker/daemon.json
+
+
+
+sudo service docker restart
+
