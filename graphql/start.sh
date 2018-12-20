f4633cb6f608eb9fc424b36ea52457e301da838a
gombocz@student.tugraz.at <gombocz@student.tugraz.at>
2018-12-20 13:46:00 +0100

    added startup for hasura, nothing more

---
+++ graphql/start.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+docker run -d -p 11443:8080 \
+       --network=SA \
+       -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:postgres@postgis:5432/postgres \
+       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
+       hasura/graphql-engine:v1.0.0-alpha33
