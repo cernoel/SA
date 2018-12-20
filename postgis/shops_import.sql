278338038108484863a8766418a5e7d6689f5448
gombocz@student.tugraz.at <gombocz@student.tugraz.at>
2018-12-20 10:13:27 +0100

    redone the config, but network access link is not working properly

--- postgis/shops_import.sql
+++ postgis/shops_import.sql
@@ -1,8 +1,3 @@
-
-DROP TABLE public.shop;
-
-DROP TABLE public.poi;
-
 CREATE TABLE 
   public.shop(
       id character varying(20)
@@ -28,8 +23,8 @@ CREATE TABLE
     );
 
 -- COPY csv to table
-\copy shop(id, lon, lat, shop, name, city, postcode, street, housenumber, website, email, phone) FROM 'shops.csv' with (format csv,header true,delimiter '|');
+\copy shop(id, lon, lat, shop, name, city, postcode, street, housenumber, website, email, phone) FROM '/tmp/shops.csv' with (format csv,header true,delimiter '|');
 
-\copy poi(id, lat, lon, name) FROM 'graz_poi_complete.csv' with (format csv,header true,delimiter '|');
+\copy poi(id, lat, lon, name) FROM '/tmp/graz_poi_complete.csv' with (format csv,header true,delimiter '|');
 
 
