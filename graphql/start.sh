2322fb860789520b9458ebfd962a1e5ee1f7217d
azure_cernoel <gombocz@student.tugraz.at>
2018-12-20 13:02:12 +0000

    fixed a bug

--- graphql/start.sh
+++ graphql/start.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 docker run -d -p 11443:8080 \
+       --name=graphql \
        --network=SA \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:postgres@postgis:5432/postgres \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
