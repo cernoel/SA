b8e90a7c527f487c0d43ff8aa3df8e0dfcedf64d
azure_cernoel <gombocz@student.tugraz.at>
2018-12-20 20:59:49 +0000

    added some nifty views

--- postgis/shops_import.sql
+++ postgis/shops_import.sql
@@ -46,3 +46,53 @@ CREATE FUNCTION shopsbypoi(text, int) RETURNS TABLE(id bigint, lon real, lat rea
 		(SELECT ST_Point(poi.lon, poi.lat) FROM public.poi WHERE poi.id = $1)::geometry, $2, true)
 		$$
     LANGUAGE SQL;
+
+
+CREATE VIEW public.poi_geo AS
+ SELECT 
+	poi.id,
+    	public.st_point((poi.lon)::double precision, (poi.lat)::double precision) AS st_point
+FROM 
+	public.poi;
+
+
+CREATE VIEW public.shop_geo AS
+SELECT 
+	shop.id,
+    	public.st_point((shop.lon)::double precision, (shop.lat)::double precision) AS st_point
+FROM 
+	public.shop;
+
+
+CREATE VIEW public.shop_poi_distance AS
+SELECT 
+	s.id AS shopid,
+    	p.id AS poiid,
+    	public.st_distance_sphere(s.st_point, p.st_point) AS distance
+FROM 
+	public.poi_geo p,
+    	public.shop_geo s;
+
+CREATE VIEW public.poi_shop_distance_data AS 
+SELECT 
+	poi.name as poiname,
+	shop.lon as lon,
+	shop.lat as lat,
+	shop.name as shopname, 
+	shop.shop as shoptype,
+	shop.city as city,
+	shop.postcode as postcode,
+	shop.street as street,
+	shop.housenumber as housenumber,
+	shop.website as website,
+	shop.email as email,
+	shop.phone as phone,
+	shop_poi_distance.distance as distance
+FROM 
+	shop_poi_distance, 
+	shop, 
+	poi
+WHERE 
+	shop_poi_distance.shopid = shop.id 
+AND     shop_poi_distance.poiid = poi.id;
+
