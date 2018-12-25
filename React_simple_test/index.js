7899ee08ded17ce29a87d4993725d3104fac2d2a
Markus-MS <45643263+Markus-MS@users.noreply.github.com>
2018-12-25 13:48:52 +0100

    Add files via upload
    Simple files to test if React works and to play around with it

---
+++ React_simple_test/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./components/counter";
+
+const element = <h1>Helldsfo World</h1>;
+ReactDOM.render(<Counter />, document.getElementById("root"));
