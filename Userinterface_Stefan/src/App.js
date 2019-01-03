c81a1602ebb15eba9a9744fd9a336ae99ea2836f
Stefan <stefan.kocher@student.tugraz.at>
2019-01-03 12:20:46 +0100

    Aufgeräumte version von meinem Userinterface

--- Userinterface_Stefan/src/App.js
+++ Userinterface_Stefan/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import Done from './Done';
-import FillSearch from './Search';
-import Favorite from './favorites';
-import Buttons from './Buttons';
+import Done from './Components/Done';
+import FillSearch from './Components/Search';
+import Favorite from './Components/Favorites';
+import Buttons from './Components/Buttons';
 
 //import ApolloClient from 'apollo-boost';
 
@@ -12,9 +12,11 @@ import Buttons from './Buttons';
 
 
 class App extends Component {
+  
+  //initilise necessary Components
   state = {
-    persons: [
-      { name: 'enter name', category: 'enter category', poid: '1', distance: '100', id: '1' }
+    search_array: [
+      { name: 'enter name', category: 'enter category', poid: '', distance: '100', id: '1' }
     ],
     favorites: [{ name: '', category: '', poid: '', id: '1' }],
     showEditor: false,
@@ -22,30 +24,40 @@ class App extends Component {
   }
 
 
+  //uptade the array if you change something in the textfield 
   searchNameChangedHandler = (event) => {
     this.setState({
-      persons: [{
+      search_array: [{
         name: event.target.value,
-        category: this.state.persons[0].category,
-        distance: this.state.persons[0].distance
+        category: this.state.search_array[0].category,
+        distance: this.state.search_array[0].distance
       }]
     })
   }
 
   searchCategoryChangedHandler = (event) => {
     this.setState({
-      persons: [{ name: this.state.persons[0].name, category: event.target.value, distance: this.state.persons[0].distance}]
+      search_array: [{
+        name: this.state.search_array[0].name,
+        category: event.target.value,
+        distance: this.state.search_array[0].distance
+      }]
     })
   }
 
+
   changedDistanceHandler = (event) => {
     this.setState({
-    persons: [{ name: this.state.persons[0].name,  category: this.state.persons[0].category,   distance: event.target.value }]
-  })
+      search_array: [{
+        name: this.state.search_array[0].name,
+        category: this.state.search_array[0].category,
+        distance: event.target.value
+      }]
+    })
   }
+  //------------------------------------------------------------------
 
-
-
+  //creates the Cookie 
   createCookie = (name, favorites) => {
 
     console.log(favorites)
@@ -53,6 +65,7 @@ class App extends Component {
     document.cookie = name + "=" + favorites + "; path=/";
   }
 
+  //retrives the Cookie 
   getCookie = (c_name) => {
     let c_start = 0;
     let c_end = 0;
@@ -71,32 +84,26 @@ class App extends Component {
   }
 
 
-
+  // changes View from Search to Favorites
+  // and gets the Cookie and saves it into the array
   toggleFavoriteEditorHandler = (event, favorites) => {
-    console.log(favorites);
-    
     var json_str = this.getCookie('mycookie');
-    if(json_str != ""){
-    var fave = JSON.parse(json_str);
-    this.setState({ favorites: fave })
-    console.log("cookie", fave);
-  }
+    if (json_str != "") {
+      var fave = JSON.parse(json_str);
+      this.setState({ favorites: fave })
+    }
     let showEditorBool = this.state.showEditor;
-   
     this.setState({ showEditor: !showEditorBool })
     this.setState({ showSearch: showEditorBool })
   }
 
-
-  toggleDoneHandler = (event, favorites) => { 
-
+  // changes View from Favorites to Search
+  // and gets the Cookie and saves it into the array
+  toggleDoneHandler = (event, favorites) => {
     var json_str = JSON.stringify(favorites);
     this.createCookie('mycookie', json_str);
-    console.log("jsonstr",json_str);
-
     var json_str = this.getCookie('mycookie');
     var fave = JSON.parse(json_str);
-    console.log("cookie", fave);
 
     let showEditorBool = this.state.showEditor;
     this.setState({ favorites: fave })
@@ -104,31 +111,33 @@ class App extends Component {
     this.setState({ showSearch: showEditorBool })
   }
 
-
+  //Saves the Content of the to the Favorites Array 
+  //Save the Favorite Array as Cookie
   toggleFavoriteSaveHandler = (event, favorites, FaveToSave) => {
-   
-    console.log("FAVE TO SAVE",FaveToSave.name);
     var fave = favorites;
     var id;
-    id = favorites.length+1;
-    
-    var compare = fave[id-2].id;
-    while(id <= compare )
-    {
+    id = favorites.length + 1;
+
+    var compare = fave[id - 2].id;
+    while (id <= compare) {
       ++id;
-      if(id <= favorites.lengt){
+      if (id <= favorites.lengt) {
         break;
       }
     }
-    fave.push({name : FaveToSave.name, category: FaveToSave.category, poid : FaveToSave.poid, distance: FaveToSave.distance,id : id.toString() });
-    console.log("FAVE TO SAVE",fave);
+    fave.push({
+      name: FaveToSave.name,
+      category: FaveToSave.category,
+      poid: FaveToSave.poid,
+      distance: FaveToSave.distance,
+      id: id.toString()
+    });
     this.setState({ favorites: fave })
-
     var json_str = JSON.stringify(favorites);
     this.createCookie('mycookie', json_str);
-
-
   }
+
+  //Deletes Favorite from Favorite Array
   deleteFavoriteHandler = (favoriteIndex) => {
     console.log(favoriteIndex);
     const favorites = [...this.state.favorites];
@@ -138,86 +147,50 @@ class App extends Component {
   }
 
 
-  //the event handlers to input the searchdata
+  //to change a name in the Favorites Array
   FavoriteNameChangedHandler = (event, id) => {
-    { console.log(id) }
-   
-
     const favoritesIndex = this.state.favorites.findIndex(f => {
       return f.id === id;
     });
-    { console.log(favoritesIndex) }
-
     const favorite = {
       ...this.state.favorites[favoritesIndex]
     };
-    { console.log(favorite) }
-    { console.log(event.target.value) }
     favorite.name = event.target.value;
-
     const favorites = [...this.state.favorites];
-    { console.log(favorites) }
-
     favorites[favoritesIndex] = favorite;
-    { console.log("favorites[faveorited index]:: ") }
-    { console.log(favorites[favoritesIndex]) }
     this.setState({ favorites: favorites })
   }
 
-
+  //to change a Category in the Favorites Array
   FavoriteCategoryChangedHandler = (event, id) => {
-    { console.log(id) }
     const favoritesIndex = this.state.favorites.findIndex(f => {
       return f.id === id;
     });
-
-    { console.log("index", favoritesIndex) }
-
     const favorite = {
       ...this.state.favorites[favoritesIndex]
     };
-    { console.log(favorite) }
-    { console.log(event.target.value) }
     favorite.category = event.target.value;
-
     const favorites = [...this.state.favorites];
-    { console.log(favorites) }
-
     favorites[favoritesIndex] = favorite;
-    { console.log("favorites[faveorited index]:: ") }
-    { console.log(favorites[favoritesIndex]) }
     this.setState({ favorites: favorites })
   }
 
-
+  //to change a Distance in the Favorites Array
   FavoriteDistanceChangedHandler = (event, id) => {
-    { console.log(id) }
     const favoritesIndex = this.state.favorites.findIndex(f => {
       return f.id === id;
     });
-
-    { console.log("index", favoritesIndex) }
-
     const favorite = {
       ...this.state.favorites[favoritesIndex]
     };
-    { console.log(favorite) }
-    { console.log(event.target.value) }
     favorite.distance = event.target.value;
-
     const favorites = [...this.state.favorites];
-    { console.log(favorites) }
-
     favorites[favoritesIndex] = favorite;
-    { console.log("favorites[faveorited index]:: ") }
-    { console.log(favorites[favoritesIndex]) }
     this.setState({ favorites: favorites })
   }
 
 
-
-
-  //"renders the content to the Display"
+  //"Builds the Search components or null"
   render() {
     const style = {
       backgroundColor: 'withe',
@@ -229,7 +202,7 @@ class App extends Component {
     const searchStyle = {
       width: '265px'
     }
-    
+
     let display_search = null;
     if (this.state.showSearch) {
       display_search = (
@@ -237,22 +210,22 @@ class App extends Component {
           <h1>Deine Webapp für die Shopsuche in Graz</h1>
 
           <FillSearch
-            name={this.state.persons[0].name}
-            category={this.state.persons[0].category}
-            key={this.state.persons[0].id}
-            poid={this.state.persons[0].poid}
-            distance = {this.state.persons[0].distance}
-            
-            searchDistanceChanged = {this.changedDistanceHandler}
+            name={this.state.search_array[0].name}
+            category={this.state.search_array[0].category}
+            key={this.state.search_array[0].id}
+            poid={this.state.search_array[0].poid}
+            distance={this.state.search_array[0].distance}
+
+            searchDistanceChanged={this.changedDistanceHandler}
             searchNameChanged={this.searchNameChangedHandler}
             searchCategoryChanged={this.searchCategoryChangedHandler}>
-           </FillSearch>
+          </FillSearch>
           <button style={style} >Search</button>
 
           <br />
-          
+
           <Buttons
-            saveFavorite={(event) => this.toggleFavoriteSaveHandler(event, this.state.favorites, this.state.persons[0])}
+            saveFavorite={(event) => this.toggleFavoriteSaveHandler(event, this.state.favorites, this.state.search_array[0])}
             editFavorites={(event) => this.toggleFavoriteEditorHandler(event, this.state.favorites)}
           ></Buttons>
           {console.log("favorite", this.state.favorites)}
@@ -260,7 +233,7 @@ class App extends Component {
       )
     }
 
-
+  {/*"Builds the Favorite Editor components or null"*/}
     let display_favorites_editor = null;
     if (this.state.showEditor) {
       display_favorites_editor = (
@@ -277,39 +250,32 @@ class App extends Component {
               category={favorites.category}
               key={favorites.id}
               poid={favorites.poid}
-              distance ={favorites.distance}
+              distance={favorites.distance}
               nameChanged={(event) => this.FavoriteNameChangedHandler(event, favorites.id)}
               categoryChanged={(event) => this.FavoriteCategoryChangedHandler(event, favorites.id)}
               distanceChanged={(event) => this.FavoriteDistanceChangedHandler(event, favorites.id)}
               delete={(event) => this.deleteFavoriteHandler(index)}>
             </Favorite>
-            
+
           })}
-         { console.log("tsetsetsetset")}
-         <Done
+
+          <Done
             done={(event) => this.toggleDoneHandler(event, this.state.favorites)}
           ></Done>
-         
+
         </div>
       );
     }
+
+    //Actually displas the Builds/Content
     return (
       <div className="App">
         {display_search}
         {display_favorites_editor}
       </div>
-
     );
   }
 }
 
 export default App;
 
-
-// click={() => this.deleteFavoriteHandler(Index)}
-  //  <button style={style} onClick={this.toggleFavoriteEditorHandler}>Done</button>
-
-  //<h1>edit your Favorites</h1>
-  //{console.log("testtest")}
-  //<button >Delete</button>
-  //changed={(event) => this.FavoriteChangedHandler(event, favorite.id)}/>
