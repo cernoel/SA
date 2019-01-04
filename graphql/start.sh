f4c29c449c55e17f99de696c15a45e6e6e1f502f
gombocz@student.tugraz.at <gombocz@student.tugraz.at>
2019-01-04 17:03:45 +0100

    added apollo test project to sa-ui, testing the docker react system

--- graphql/start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-docker run -d -p 11443:8080 \
-       --name=graphql \
-       --network=SA \
-       -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:postgres@postgis:5432/postgres \
-       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       hasura/graphql-engine:v1.0.0-alpha33
